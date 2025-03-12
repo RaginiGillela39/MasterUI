fae3335669c1e14f606565039a6dba7f03486f56
Ragini-gillela <mraginireddy@gmail.com>
2025-03-12 10:07:46 +0530

    Intial Commit

--- Backend/Organization_Core/obj/Release/net8.0/TQMS_Admin_Domain.AssemblyInfo.cs
+++ Backend/Organization_Core/obj/Release/net8.0/TQMS_Admin_Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TQMS_Admin_Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37ae133a260c8553ec2ef740f239d5eb207fcfb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TQMS_Admin_Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TQMS_Admin_Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
